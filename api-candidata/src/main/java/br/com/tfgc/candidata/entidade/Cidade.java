e1353477bb6eb1fbf23f8621a1665a85ba467630
noemialves <noemi.alves@live.com>
2018-09-16 06:23:16 -0300

    Alterações de Merge

---
+++ api-candidata/src/main/java/br/com/tfgc/candidata/entidade/Cidade.java
@@ -0,0 +1,48 @@
+package br.com.tfgc.candidata.entidade;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cidade")
+public class Cidade {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "nome", nullable = true)
+	private String nome;
+
+	@Column(name = "estado", nullable = true)
+	private String estado;
+
+	public Cidade(String nomeCidade, String nomeEstado) {
+		this.nome = nomeCidade;
+		this.estado = nomeEstado;
+	}
+
+	public Cidade() {
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nomeCidade) {
+		this.nome = nomeCidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String nomeEstado) {
+		this.estado = nomeEstado;
+	}
+}
